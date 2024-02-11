580322db49959c2dee2d88fa0f13ede519ef9979
Émile Siou <104312278+Sooey-99@users.noreply.github.com>
2024-02-11 19:26:48 +0000

    Added js files and js questions

---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+// variables for questions
+
+let questionList = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["a. HyperText Markup Language", "b. High-Level Text Management Language", "c. Hyper Transfer Markup Language", "d. HyperText Management Language"],
+        correctAnswer: 0,
+    },
+    {
+        title: "What is the purpose of the HTML <head> element?",
+        choices: ["a. It defines the main content of the HTML document", "b. It contains metadata about the HTML document", "c. It represents the main heading of the document", "d. It defines a section in the document"],
+        correctAnswer: 1,
+    },
+    {
+        title: "How can you apply an external style sheet to an HTML document?",
+        choices: ["a. <style> tag", "c. <css> tag", "b. <link> tag", "d. <script> tag"],
+        correctAnswer: 2,
+    },
+    {
+        title: "What is the purpose of the document.getElementById() method in JavaScript?",
+        choices: ["a. It gets the value of an input field", "b. It retrieves an HTML element by its ID", "c. It sets the value of an attribute", "d. It creates a new HTML element"],
+        correctAnswer: 1,
+    },
+];
