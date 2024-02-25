6e603954fb185c0f7a0d5542bf3241f941956ca4
Émile Siou <104312278+Sooey-99@users.noreply.github.com>
2024-02-25 18:43:33 +0000

    updated questions in questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,42 +1,52 @@
 let questionList = [
     {
-        title: "What does HTML stand for?",
+        title: "What is the purpose of the CSS 'margin' property?",
         choices: [
-            "a. HyperText Markup Language",
-            "b. High-Level Text Management Language",
-            "c. Hyper Transfer Markup Language",
-            "d. HyperText Management Language"
+            "a. Sets the spacing between elements' content and border",
+            "b. Defines the width of an element",
+            "c. Changes the background color of an element",
+            "d. Specifies the font size of the text"
         ],
         correctAnswer: 0,
     },
     {
-        title: "What is the purpose of the HTML <head> element?",
+        title: "In HTML, what does the 'alt' attribute in the 'img' tag stand for?",
         choices: [
-            "a. It defines the main content of the HTML document",
-            "b. It contains metadata about the HTML document",
-            "c. It represents the main heading of the document",
-            "d. It defines a section in the document"
+            "a. Alternative",
+            "b. Altitude",
+            "c. Alter",
+            "d. Attribute"
+        ],
+        correctAnswer: 0,
+    },
+    {
+        title: "Which HTML tag is used to link an external JavaScript file?",
+        choices: [
+            "a. <link>",
+            "b. <script>",
+            "c. <js>",
+            "d. <source>"
         ],
         correctAnswer: 1,
     },
     {
-        title: "How can you apply an external style sheet to an HTML document?",
+        title: "What does the CSS property 'display: flex;' do?",
         choices: [
-            "a. <style> tag",
-            "b. <link> tag",
-            "c. <css> tag", // corrected order
-            "d. <script> tag"
+            "a. Sets an element to be block-level",
+            "b. Aligns elements horizontally in a row",
+            "c. Hides an element",
+            "d. Adds a shadow to an element"
+        ],
+        correctAnswer: 1,
+    },
+    {
+        title: "How do you comment in JavaScript?",
+        choices: [
+            "a. <!-- This is a comment -->",
+            "b. // This is a comment",
+            "c. /* This is a comment */",
+            "d. -- This is a comment --"
         ],
         correctAnswer: 2,
-    },
-    {
-        title: "What is the purpose of the document.getElementById() method in JavaScript?",
-        choices: [
-            "a. It gets the value of an input field",
-            "b. It retrieves an HTML element by its ID",
-            "c. It sets the value of an attribute",
-            "d. It creates a new HTML element"
-        ],
-        correctAnswer: 1,
     },
 ];
